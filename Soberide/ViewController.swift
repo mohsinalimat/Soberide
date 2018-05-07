c1310980346e5ff0014e546099db2a0adcf4eaf6
Grant Parton <granteparton@gmail.com>
2018-05-07 14:01:40 -0700

    Created project file

---
+++ Soberide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soberide
+//
+//  Created by Grant Parton on 5/7/18.
+//  Copyright © 2018 Grant Parton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
