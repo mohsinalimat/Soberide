94f9ccfe9e7a86722e659939757449db7f16ccc6
Grant Parton <granteparton@gmail.com>
2018-05-28 16:12:05 -0700

    Added more views, and firebase files + connection in appdelegate

---
+++ Soberide/AdminView.swift
@@ -0,0 +1,9 @@
+//
+//  AdminView.swift
+//  Soberide
+//
+//  Created by Grant Parton on 5/25/18.
+//  Copyright © 2018 Grant Parton. All rights reserved.
+//
+
+import Foundation
---
+++ Soberide/RegisterGroupView.swift
@@ -0,0 +1,27 @@
+//
+//  RegisterView.swift
+//  Soberide
+//
+//  Created by Grant Parton on 5/25/18.
+//  Copyright © 2018 Grant Parton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterGroupView: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
